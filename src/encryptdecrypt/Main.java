64a03f55c9f69020fd88cac3783d53ee2d540eea
Matvey Karachun <karachun.m@mail.ru>
2021-12-15 22:59:32 +0300

    added args -in, -out

--- src/encryptdecrypt/Main.java
+++ src/encryptdecrypt/Main.java
@@ -1,15 +1,25 @@
 package encryptdecrypt;
 
+import encryptdecrypt.strategies.CaesarCipher;
 import encryptdecrypt.strategies.CaesarCipherUnicode;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.function.UnaryOperator;
 
 public class Main {
     public static void main(String[] args) {
 
-        boolean encMode = true;
         int key = 0;
         String data = "";
+        String inPath = "";
+        String outPath = "";
+
+        boolean encMode = true;
+        boolean thereIsData = false;
+        boolean thereIsInPath = false;
+        boolean thereIsOutPath = false;
 
         for (int i = 0; i < args.length - 1; i += 2) {
             switch (args[i]) {
@@ -23,6 +33,15 @@ public class Main {
                     break;
                 case "-data":
                     data = args[i + 1];
+                    thereIsData = true;
+                    break;
+                case "-in":
+                    inPath = args[i];
+                    thereIsInPath = true;
+                    break;
+                case "-out":
+                    outPath = args[i];
+                    thereIsOutPath = true;
                     break;
             }
         }
@@ -31,8 +50,20 @@ public class Main {
             key = -key;
         }
 
+        if (thereIsInPath && !thereIsData) {
+            try {
+                data = new String(Files.readAllBytes(Paths.get(inPath)));
+            } catch (IOException e) {
+                System.out.println("Error: can't read file");
+            }
+        }
 
-        UnaryOperator<String> encryptor = new CaesarCipherUnicode(key)::apply;
-        System.out.println(encryptor.apply(data));
+        CaesarCipher cipher = new CaesarCipherUnicode(key);
+        String result = cipher.apply(data);
+        if (thereIsOutPath) {
+
+        } else {
+            System.out.println(result);
+        }
     }
 }
