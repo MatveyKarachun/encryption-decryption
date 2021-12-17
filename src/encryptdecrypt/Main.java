bed443975049117bab7dca5b34009d4e6af865ea
Matvey Karachun <karachun.m@mail.ru>
2021-12-17 21:40:30 +0300

    changed reading of file

--- src/encryptdecrypt/Main.java
+++ src/encryptdecrypt/Main.java
@@ -7,7 +7,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
@@ -53,7 +53,7 @@ public class Main {
 
         if (thereIsInPath && !thereIsData) {
             try {
-                data = new String(Files.readAllBytes(Paths.get(inPath)));
+                data = Files.readString(Path.of(inPath));
             } catch (IOException e) {
                 System.out.println("Error: can't read file");
                 System.exit(0);
